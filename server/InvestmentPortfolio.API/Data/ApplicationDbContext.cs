5b0a2e19e5b304b83359e960d702fe1ee08c4dfd
Bouchaib Chraf <135054721+bchraf3@users.noreply.github.com>
2025-03-07 19:31:03 +0200

    Configure entity relationships in ApplicationDbContext

--- server/InvestmentPortfolio.API/Data/ApplicationDbContext.cs
+++ server/InvestmentPortfolio.API/Data/ApplicationDbContext.cs
@@ -10,17 +10,52 @@ public class ApplicationDbContext : DbContext
     {
     }
 
-public DbSet<Portfolio> Portfolios { get; set; } = null!;
-public DbSet<Investment> Investments { get; set; } = null!;
+    public DbSet<ApplicationUser> Users { get; set; } = null!;
+    public DbSet<Portfolio> Portfolios { get; set; } = null!;
+    public DbSet<Investment> Investments { get; set; } = null!;
+    public DbSet<InvestmentProfile> InvestmentProfiles { get; set; } = null!;
+    public DbSet<UserActivity> UserActivities { get; set; } = null!;
+    public DbSet<UserPreferences> UserPreferences { get; set; } = null!;
+    public DbSet<WatchlistItem> WatchlistItems { get; set; } = null!;
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         
-        // Define relationships
+        // Investment to Portfolio relation (already exists)
         modelBuilder.Entity<Investment>()
             .HasOne(i => i.Portfolio)
             .WithMany(p => p.Investments)
             .HasForeignKey(i => i.PortfolioId);
+            
+        // User to Portfolio relation
+        modelBuilder.Entity<Portfolio>()
+            .HasOne(p => p.User)
+            .WithMany(u => u.Portfolios)
+            .HasForeignKey(p => p.UserId);
+            
+        // User to UserPreferences (one-to-one)
+        modelBuilder.Entity<UserPreferences>()
+            .HasOne(up => up.User)
+            .WithOne(u => u.Preferences)
+            .HasForeignKey<UserPreferences>(up => up.UserId);
+            
+        // User to InvestmentProfile (one-to-one)
+        modelBuilder.Entity<InvestmentProfile>()
+            .HasOne(ip => ip.User)
+            .WithOne(u => u.InvestmentProfile)
+            .HasForeignKey<InvestmentProfile>(ip => ip.UserId);
+            
+        // User to WatchlistItem
+        modelBuilder.Entity<WatchlistItem>()
+            .HasOne(wi => wi.User)
+            .WithMany(u => u.Watchlist)
+            .HasForeignKey(wi => wi.UserId);
+            
+        // User to UserActivity
+        modelBuilder.Entity<UserActivity>()
+            .HasOne(ua => ua.User)
+            .WithMany(u => u.ActivityHistory)
+            .HasForeignKey(ua => ua.UserId);
     }
 }
