d8f6a77be4d2c9bda1335aff01a7cd76c6f075f9
Bouchaib Chraf <135054721+bchraf3@users.noreply.github.com>
2025-03-13 18:55:53 +0200

    feat(auth): Create Auth Buttons

---
+++ client/src/components/buttons/logout-button.tsx
@@ -0,0 +1,23 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+export const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
+
+  return (
+    <button
+      className="btn btn-danger"
+      onClick={handleLogout}
+    >
+      Log Out
+    </button>
+  );
+};
---
+++ client/src/components/buttons/signup-button.tsx
@@ -0,0 +1,26 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+export const SignupButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleSignUp = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/dashboard",
+      },
+      authorizationParams: {
+        screen_hint: "signup",
+      },
+    });
+  };
+
+  return (
+    <button
+      className="btn bg-secondary-600 text-white hover:bg-secondary-700"
+      onClick={handleSignUp}
+    >
+      Sign Up
+    </button>
+  );
+};
